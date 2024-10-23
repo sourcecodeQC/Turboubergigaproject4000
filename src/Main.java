18350a07b1b0117f8bbfb3ea8f6f4cb9d3ee1c0d
scQC <alek.de.mehlem@gmail.com>
2024-10-23 14:02:38 +0200

    guthib

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+//library book manager project
+
+import java.util.*;
+import java.io.*;
+import java.nio.file.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        //file
+
+
+
+
+
+
     }
 }
